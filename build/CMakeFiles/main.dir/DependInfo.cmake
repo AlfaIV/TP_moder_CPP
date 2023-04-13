17470de0761e4f6ba6cde46d42a528c3ffb098a9
AlfaIV <AlfaIV@yandex.ru>
2023-04-13 10:39:14 +0300

    modify readme; easy pipline test

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alfaiv/Code/TP_VK/modern_cpp/HM1/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alfaiv/Code/TP_VK/modern_cpp/HM1/build/CMakeFiles/CLIcommands.dir/DependInfo.cmake"
+  "/home/alfaiv/Code/TP_VK/modern_cpp/HM1/build/CMakeFiles/handler.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
